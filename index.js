ab26df66f3de170e936cf5443df1e137f2ecf918
Nathaniel <nathaniel.jaden@student.moringaschool.com>
2024-04-03 12:26:00 +0300

    Sending Data with Fetch

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+function submitData(name, email) {
+  const data = {
+    name: name,
+    email: email,
+  };
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      document.body.innerHTML = data.id;
+    })
+    .catch((error) => {
+      document.body.innerHTML = error.message;
+    });
+}
